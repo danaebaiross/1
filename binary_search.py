6853df1f2fb494202e2ffdd97eb2cc3b9b1d75c4
Timofey <123194387+danaebaiross@users.noreply.github.com>
2023-12-03 17:26:08 +0300

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = binary_search(my_list, 5)
+print("index 5:", result)
